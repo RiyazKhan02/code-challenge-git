ca340b8f4c4b77fc88f0c1ae298395d993b78fdd
RiyazKhan02 <144216455+RiyazKhan02@users.noreply.github.com>
2023-09-10 22:16:16 +1000

    Update GitDataControllerTest.java

--- spring-boot-coding-exercise/microservice/src/test/java/com/telstra/codechallenge/gitdata/controller/GitDataControllerTest.java
+++ spring-boot-coding-exercise/microservice/src/test/java/com/telstra/codechallenge/gitdata/controller/GitDataControllerTest.java
@@ -6,14 +6,12 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
